df2603220d5f9677d1bb5cf8484f125ac0f350f8
lulavalenca <lulavalencafilho@gmail.com>
2023-06-06 20:43:23 -0300

    meus primeiros codigos

---
+++ ex1.py
@@ -0,0 +1,3 @@
+print("Crie um programa, Ola mundo")
+mensagem = input("Digite uma mensagem: ")
+print("Você digitou:", mensagem)
---
+++ ex2.py
@@ -0,0 +1,4 @@
+print("digite um numero inteiro para retornar o dobro")
+numero = int(input("Digite um numero inteiro: "))
+dobro = numero * 2
+print("o dobro do numero é:", dobro)
