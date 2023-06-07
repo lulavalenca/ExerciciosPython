8357211e1b1563fa41219e50874012eb21e6edb0
lulavalenca <lulavalencafilho@gmail.com>
2023-06-06 21:05:32 -0300

    codigo do exercicio 3

--- ex3.py
+++ ex3.py
@@ -1,1 +1,8 @@
 
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+media = (numero1 + numero2 + numero3) / 3
+
+print("A média aritmética dos números é:", media)
